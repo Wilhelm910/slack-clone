464f02e3673d93b0a91baa5ca73dda1ae264bd40
Ingo <ingohermsen@gmail.com>
2023-06-08 17:23:22 +0200

    created sign up form an implementend google auth

--- src/app/components/main/main/main.component.ts
+++ src/app/components/main/main/main.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
