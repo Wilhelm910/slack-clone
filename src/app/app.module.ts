2e2a6841d5c710d002e816843e62d94ea7951840
Ingo <ingohermsen@gmail.com>
2023-06-08 13:05:14 +0200

    registered common primeng modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,45 @@
+//Angular Fire
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
+//UI Library
+import { MenuModule } from 'primeng/menu';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { MessagesModule } from 'primeng/messages';
+import { CardModule } from 'primeng/card';
+import { DropdownModule } from 'primeng/dropdown';
+import { AvatarModule } from 'primeng/avatar';
+import { DragDropModule } from 'primeng/dragdrop';
+import { SplitButtonModule } from 'primeng/splitbutton';
+
+//General
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { LoginDialogComponent } from './auth/login-dialog/login-dialog.component';
-import { SignUpDialogComponent } from './auth/sign-up-dialog/sign-up-dialog.component';
-import { ForgotPasswordDialogComponent } from './auth/forgot-password-dialog/forgot-password-dialog.component';
+
+import { LoginDialogComponent } from './components/auth/login-dialog/login-dialog.component';
+import { SignUpDialogComponent } from './components/auth/sign-up-dialog/sign-up-dialog.component';
+import { ForgotPasswordDialogComponent } from './components/auth/forgot-password-dialog/forgot-password-dialog.component';
+import { AuthComponent } from './components/auth/auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +47,29 @@ import { ForgotPasswordDialogComponent } from './auth/forgot-password-dialog/for
     LoginDialogComponent,
     SignUpDialogComponent,
     ForgotPasswordDialogComponent,
+    AuthComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MenuModule,
+    DialogModule,
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    CheckboxModule,
+    RadioButtonModule,
+    SidebarModule,
+    MultiSelectModule,
+    CalendarModule,
+    SelectButtonModule,
+    MessagesModule,
+    CardModule,
+    DropdownModule,
+    AvatarModule,
+    DragDropModule,
+    SplitButtonModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
