121c668d95fefbbd7c06306f37aa2c90168f7bc1
Ingo <ingohermsen@gmail.com>
2023-06-08 10:49:39 +0200

    folder structure and first auth components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginDialogComponent } from './auth/login-dialog/login-dialog.component';
+import { SignUpDialogComponent } from './auth/sign-up-dialog/sign-up-dialog.component';
+import { ForgotPasswordDialogComponent } from './auth/forgot-password-dialog/forgot-password-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDialogComponent,
+    SignUpDialogComponent,
+    ForgotPasswordDialogComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-clone-8b87c',
+    appId: '1:108302256805:web:088a15f685a87fb533db4c',
+    storageBucket: 'slack-clone-8b87c.appspot.com',
+    apiKey: 'AIzaSyDaZSWU999f8GhlxqR946lv2-wJWgIDkOM',
+    authDomain: 'slack-clone-8b87c.firebaseapp.com',
+    messagingSenderId: '108302256805',
+  },
+};
