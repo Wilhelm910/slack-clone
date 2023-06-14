be2317a11c6e18bb5e6770a816b4b9c3369795d9
Ingo <ingohermsen@gmail.com>
2023-06-14 19:11:45 +0200

    updated channel object (removed messages-array)

--- src/app/core/models/channel.class.ts
+++ src/app/core/models/channel.class.ts
@@ -2,13 +2,11 @@ export class Channel {
   // public cId: string;
   public name: string;
   // public users: string[] = [];
-  public messages: string[] = [];
 
 
   constructor(obj?: any) {
     
     this.name = obj ? obj.name : '';
-    this.messages = obj ? obj.messages : '';
   }
 
 
@@ -17,7 +15,6 @@ export class Channel {
       //    cId: this.cId,
       name: this.name,
       //  users: this.users,
-      messages: this.messages
     }
   }
 }
