1951d04f715467b980bf4b198626ea7fd5c05c2e
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2023-06-18 15:26:06 +0200

    create-chat component update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { SignUpDialogComponent } from './components/auth/sign-up-dialog/sign-up-
 import { MainComponent } from './components/main/main/main.component';
 import { ChannelDetailComponent } from './components/main/channel-detail/channel-detail.component';
 import { LegalComponent } from './components/main/legal/legal.component';
+import { CreateChannelComponent } from './components/main/create-channel/create-channel.component';
+import { CreateChatComponent } from './components/main/create-chat/create-chat.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full'},
@@ -23,6 +25,7 @@ const routes: Routes = [
   {
     path: '', component: MainComponent, children: [
       { path: 'channel/:id', component: ChannelDetailComponent },
+      { path: 'createChat', component: CreateChatComponent },
     ]
   },
 ];
--- src/app/components/main/create-chat/create-chat.component.ts
+++ src/app/components/main/create-chat/create-chat.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { User } from 'src/app/core/models/user.class';
 
 @Component({
   selector: 'app-create-chat',
   templateUrl: './create-chat.component.html',
   styleUrls: ['./create-chat.component.scss']
 })
-export class CreateChatComponent {
+export class CreateChatComponent implements OnInit {
+
+  allUsers = [];
+
+  constructor(private firestore: AngularFirestore) { }
+
+  ngOnInit(): void {
+    this.firestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((changes:any) => {
+        this.allUsers = changes;
+        console.log(this.allUsers)
+      })
+}
 
 }
